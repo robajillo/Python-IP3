e21f620c941bd2d5402127bdd19987dd30c95a0f
robajillo <rjillo@yahoo.com>
2020-09-18 22:59:12 +0300

    returned user class

--- manage.py
+++ manage.py
@@ -10,7 +10,7 @@ manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db)
+    return dict(app=app, db=db, User = User)
 
 
 if __name__ == '__main__':
