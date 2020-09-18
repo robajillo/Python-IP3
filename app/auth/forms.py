f1e15281ef4f6f7615bf99a1eec2f276de2a3760
robajillo <rjillo@yahoo.com>
2020-09-19 00:28:03 +0300

    added a BooleanField input field

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 from wtforms import ValidationError
