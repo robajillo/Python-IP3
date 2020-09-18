9dd895ae16a35a4aedf24e67130291d4dcfc2fe1
robajillo <rjillo@yahoo.com>
2020-09-18 23:13:31 +0300

    created a column in Users table that stores password

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
 
     def __repr__(self):
         return f'User {self.username}'
