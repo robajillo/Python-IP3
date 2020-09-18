cf4bba934583768af192c98325f1dfdd65a13d13
robajillo <rjillo@yahoo.com>
2020-09-18 23:47:45 +0300

    defined a root for main index page

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,9 @@
 from flask import render_template,request,redirect,url_for
 from . import main
 from flask_login import login_required
+
+
+@main.route('/')
+def index():
+    
+    return render_template('index.html')
