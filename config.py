86ce58bba5e79048a67458af8965fa98f42f5a18
robajillo <rjillo@yahoo.com>
2020-09-18 17:54:11 +0300

    solved errors

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://roba:marsabit@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://roba:marsabit@localhost/pitches'
     
 
 class ProdConfig(Config):
