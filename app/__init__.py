c5d68b85c75da3615220b56e2e2eb005298de079
robajillo <rjillo@yahoo.com>
2020-09-18 17:13:17 +0300

    imported sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_bs4 import Bootstrap
+from flask_sqlachemy  import SQLAlchemy
 from config import config_options
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
 
