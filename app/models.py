30f959eadcef6d481b6ee5240edf598f37c5f279
robajillo <rjillo@yahoo.com>
2020-09-18 23:18:56 +0300

    used @property decorater to create a write only class prop[erty password

--- app/models.py
+++ app/models.py
@@ -6,6 +6,17 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
         return f'User {self.username}'
