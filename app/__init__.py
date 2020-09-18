21bb0aaf31a9f52b31af87c66ae14b03b726ac23
robajillo <rjillo@yahoo.com>
2020-09-18 23:36:38 +0300

    registered a blueprint instance in create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -24,5 +24,7 @@ def create_app(config_name):
     # setting config
     # from .requests import configure_request
     # configure_request(app)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
     return app
